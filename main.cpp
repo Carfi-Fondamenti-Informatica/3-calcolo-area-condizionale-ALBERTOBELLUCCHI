1d1082cf98dd1ee46b28c6d1aade0a97e7ed5c2f
Alberto_Bellucchi <115469272+ALBERTOBELLUCCHI@users.noreply.github.com>
2022-11-17 16:11:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a, b;
+    int opzione;
+    cin>>a>>b>>opzione;//input
+
+    switch(opzione){
+        case 0://triangolo
+            cout<<0.5*a*b<<endl;
+            break;
+
+        case 1://quadrato
+            cout<<a*a<<endl;
+            break;
+
+        case 2://rettangolo
+            cout<<a*b<<endl;
+            break;
+
+        default:
+            cout<<"opzione non valida"<<endl;
+    }
+
+    return 0;
 }
